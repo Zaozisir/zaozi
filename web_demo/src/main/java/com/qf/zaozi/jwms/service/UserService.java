1140ada64b4f536cf4293463c18772f5dbc5586b
Zaozisir <130815292+Zaozisir@users.noreply.github.com>
2023-04-15 16:40:55 +0800

    Delete web_demo directory

--- web_demo/src/main/java/com/qf/zaozi/jwms/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.qf.zaozi.jwms.service;
-
-import com.qf.zaozi.jwms.pojo.ResultDate;
-
-public interface UserService {
-
-    ResultDate chek(String username, String password, String role);
-}
